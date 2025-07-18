0998342cf1471ddfd2445348cbed5735e040bd94
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2025-07-18 23:40:40 +0530

    Add Header and Footer components; update layout and page structure

---
+++ app/components/Footer.tsx
@@ -0,0 +1,5 @@
+export const Footer = () => {
+  return (
+    <footer className="border-t border-dotted border-white/20 p-12 w-full absolute bottom-0 left-0"></footer>
+  );
+};
---
+++ app/components/Header.tsx
@@ -0,0 +1,5 @@
+export const Header = () => {
+  return (
+    <nav className="border-b border-white/20 border-dotted border-border/50 p-12 absolute w-full" />
+  );
+};
